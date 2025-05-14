8fd0f6a506e17c1e2e8209d39f57c25c5bc1f4c4
SamChrstopher <samchrstopher@gmail.com>
2025-05-14 15:43:06 +0530

    todos

---
+++ src/db.ts
@@ -0,0 +1,18 @@
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv"
+
+//configuring the env
+dotenv.config()
+
+const client = new MongoClient(process.env.MONGO_URI!)
+let db: any;
+
+export async function connectDB() {
+    await client.connect()
+    db = client.db() //Connects to the default DB in MongoDB
+    console.log("MongoDB connected!")    
+}
+
+export function getDB(){
+    return db
+}
